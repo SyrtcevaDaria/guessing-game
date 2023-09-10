58f457491d38c4dcc5a08d1a99eb3adb81e77024
SyrtcevaDaria <syrtceva566333@gmail.com>
2023-09-10 11:53:27 +0300

    q

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.assumption = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        if (this.right >= this.left) {
+            this.assumption = Math.ceil((this.right - this.left) / 2) + this.left;
+        }
+        return this.assumption;
     }
 
     lower() {
+        this.right = this.assumption;
 
     }
 
     greater() {
-
+        this.left = this.assumption;
     }
 }
 
